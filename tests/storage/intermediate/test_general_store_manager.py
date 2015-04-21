6909e5fe105b122c3b01d331f72e81686a3f2e38
dpena <dpena@caltech.edu>
2015-04-20 17:40:03 -0700

    Finished tests for GeneralStoreManager
    GeneralStoreManager now has 100% coverage.

--- tests/storage/intermediate/test_general_store_manager.py
+++ tests/storage/intermediate/test_general_store_manager.py
@@ -84,3 +84,46 @@ class TestGeneralStoreManagerMethods(unittest.TestCase):
         # Read the item index to make sure the item is deleted
         item_read = store_manager.get_item_at_index(item.index)
         self.assertEquals(item_read, None)
+
+    def test_get_indexes(self):
+        """
+        Test that get indexes returns the expected number of recycled IDs or
+        last file indexes.
+        """
+        store_manager = GeneralStoreManager(self.TEST_STORE())
+        # Get an index (should be the last file index)
+        index_list = store_manager.get_indexes()
+        # Check that the last index is as expected the last file index
+        index = index_list[0]
+        self.assertEquals(index, store_manager.store.get_last_file_index())
+        # Create an item with the given index
+        store_manager.create_item(index)
+        # Delete the item, the index should now be in the recycled IDs
+        store_manager.delete_item_at_index(index)
+
+        # Get two indexes, now one should be a recycled index i.e. 1, and the
+        # other should be the last file index
+        index_list = store_manager.get_indexes(2)
+        index1 = index_list[0]
+        index2 = index_list[1]
+        # Check that the first index is the recycled one
+        self.assertEquals(index, index1)
+        # And that the last index is the last file index
+        self.assertEquals(store_manager.store.get_last_file_index(), index2)
+
+        # Create two items with the indexes
+        store_manager.create_item(index1)
+        store_manager.create_item(index2)
+        # And a third without an index argument
+        store_manager.create_item()
+        # Delete the second item (second index)
+        store_manager.delete_item_at_index(index2)
+
+        # Now when we get 4 IDs, one should be the recycled second index, and
+        # the other 3 should be the last file index with 2 increments
+        index_list = store_manager.get_indexes(4)
+        last_index = store_manager.store.get_last_file_index()
+        self.assertEquals(index_list[0], index2)
+        self.assertEquals(index_list[1], last_index)
+        self.assertEquals(index_list[2], last_index + 1)
+        self.assertEquals(index_list[3], last_index + 2)
