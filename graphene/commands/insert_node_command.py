d915585b80d7c93f3f52517e8922590c5efd53e6
Eugene Bulkin <ebulkin@caltech.edu>
2015-04-26 15:37:41 -0700

    InsertNodeCommand tests

--- graphene/commands/insert_node_command.py
+++ graphene/commands/insert_node_command.py
@@ -8,7 +8,8 @@ class InsertNodeCommand(Command):
     def get_type_type_of_string(self, s):
         if s.upper() == "TRUE" or s.upper() == "FALSE":
             return Property.PropertyType.bool
-        if s.isdigit():
+        if s.isdigit() or \
+            ((s[0] == '-' or s[0] == '+') and s[1:].isdigit()):
             return Property.PropertyType.int
         if s[0] == '"' and s[-1] == '"':
             return Property.PropertyType.string
--- tests/commands/test_insert_node_command.py
+++ tests/commands/test_insert_node_command.py
@@ -1,5 +1,6 @@
 import unittest
 
+from graphene.commands import InsertNodeCommand
 from graphene.storage import (StorageManager, GrapheneStore, Property)
 from graphene.server.server import GrapheneServer
 
@@ -20,3 +21,24 @@ class TestInsertNodeCommand(unittest.TestCase):
 
     def test_insert_single_node(self):
         assert self.server.doCommands("CREATE TYPE T ( a: int, b: string );", False)
+
+    def test_init(self):
+        cmd = InsertNodeCommand("foo")
+        assert cmd.node_prop_list == "foo"
+
+    def test_get_tt_of_string(self):
+        empty_cmd = InsertNodeCommand(None)
+        assert empty_cmd.get_type_type_of_string('"a"') == Property.PropertyType.string
+        assert empty_cmd.get_type_type_of_string('true') == Property.PropertyType.bool
+        assert empty_cmd.get_type_type_of_string('false') == Property.PropertyType.bool
+        assert empty_cmd.get_type_type_of_string('34') == Property.PropertyType.int
+        assert empty_cmd.get_type_type_of_string('3.4') == Property.PropertyType.undefined
+        assert empty_cmd.get_type_type_of_string('-34') == Property.PropertyType.int
+
+    def test_convert_to_value(self):
+        empty_cmd = InsertNodeCommand(None)
+        assert empty_cmd.convert_to_value('"a"', Property.PropertyType.string) == "a"
+        assert empty_cmd.convert_to_value('true', Property.PropertyType.bool) == True
+        assert empty_cmd.convert_to_value('false', Property.PropertyType.bool) == False
+        assert empty_cmd.convert_to_value('34', Property.PropertyType.int) == 34
+        assert empty_cmd.convert_to_value('-34', Property.PropertyType.int) == -34
