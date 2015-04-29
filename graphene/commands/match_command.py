91bd488cc3c46beb82d5025d81b602893beb5ead
Eugene Bulkin <ebulkin@caltech.edu>
2015-04-28 17:44:26 -0700

    Basic RETURN clause work

--- graphene/commands/match_command.py
+++ graphene/commands/match_command.py
@@ -5,9 +5,10 @@ from graphene.commands.command import Command
 from graphene.utils import PrettyPrinter
 
 class MatchCommand(Command):
-    def __init__(self, node_chain, query_chain):
+    def __init__(self, node_chain, query_chain, return_chain):
         self.nc = node_chain
         self.qc = query_chain or ()
+        self.rc = return_chain or ()
 
     def __repr__(self):
         lst = ["\t%s" % chain for chain in self.nc]
@@ -20,11 +21,22 @@ class MatchCommand(Command):
         type_data, type_schema = storage_manager.get_node_data(first_match.type)
         qc = Query.parse_chain(storage_manager, self.qc, type_schema, alias)
 
-        header = [name.upper() for tt, name, ttype in type_schema]
+        if len(self.rc) == 0:
+            return_filter = lambda x: x
+        else:
+            indexes = []
+            schema_names = [name for tt, name, ttype in type_schema]
+            for r in self.rc:
+                ident, name = r
+                if (ident == alias or ident is None) and name in schema_names:
+                    indexes.append(schema_names.index(name))
+            return_filter = lambda l: [l[i] for i in indexes]
+
+        header = return_filter([name.upper() for tt, name, ttype in type_schema])
         iterator = NodeIterator(storage_manager, type_data, type_schema, alias, qc)
         values = []
         for node in iterator:
-            values.append(node.properties)
+            values.append(return_filter(node.properties))
         if len(values) == 0:
             output.write("No nodes found.\n")
             return []
