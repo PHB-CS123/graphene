2285e5928b1ad31a0a5e93a58de4465039f6c229
Eugene Bulkin <ebulkin@caltech.edu>
2015-05-31 01:12:59 -0700

    Update syntax

--- graphene/commands/update_relation_command.py
+++ graphene/commands/update_relation_command.py
@@ -1,1 +1,17 @@
 __author__ = 'David'
+
+from graphene.commands.command import Command
+from graphene.expressions import *
+from graphene.traversal import Query
+from graphene.query.planner import QueryPlanner
+
+class UpdateRelationCommand(Command):
+    """
+    Used to update individual relations from the database.
+    """
+    def __init__(self, rel_type, query_chain, left_node, right_node, update):
+        self.rel_type = rel_type
+        self.qc = query_chain
+        self.left_node = left_node
+        self.right_node = right_node
+        self.update = update
