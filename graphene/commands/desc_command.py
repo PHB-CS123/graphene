158e052d1977256c624c0baecfaef2830d091a9f
Eugene Bulkin <ebulkin@caltech.edu>
2015-06-02 19:27:56 -0700

    Update DESC command to handle relations

--- graphene/commands/desc_command.py
+++ graphene/commands/desc_command.py
@@ -6,13 +6,22 @@ from graphene.errors import TypeDoesNotExistException
 from graphene.utils import PrettyPrinter
 from graphene.storage import GeneralStore
 
-class DescTypeCommand(Command):
-    def __init__(self, type_name):
+class DescCommand(Command):
+    class DescType(Enum):
+        TYPE = 1
+        RELATION = 2
+
+    def __init__(self, type_name, desc_type):
+        print type_name
         self.type_name = type_name
+        self.desc_type = desc_type
 
     def execute(self, storage_manager, output=sys.stdout):
         try:
-            type_data, type_schema = storage_manager.get_node_data(self.type_name)
+            if self.desc_type == DescCommand.DescType.TYPE:
+                type_data, type_schema = storage_manager.get_node_data(self.type_name)
+            else:
+                type_data, type_schema = storage_manager.get_relationship_data(self.type_name)
             values = [(tt_name, tt_type.name) for tt, tt_name, tt_type in type_schema]
             PrettyPrinter.print_table(values, header=("Name", "Type"), output=output)
         except TypeDoesNotExistException as e:
