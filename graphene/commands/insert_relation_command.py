9769216d5692bee4d8f231598aea1f23df2087a1
Eugene Bulkin <ebulkin@caltech.edu>
2015-06-08 14:40:59 -0700

    Make inserting relations not inefficient
    Inserting relations now only loops over types once, which makes it much faster.

--- graphene/commands/insert_relation_command.py
+++ graphene/commands/insert_relation_command.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 from graphene.commands.command import Command
 from graphene.storage import StorageManager
 from graphene.expressions import *
@@ -46,6 +44,19 @@ class InsertRelationCommand(Command):
             properties.append((given_type, conv_value))
         return properties
 
+    def prop_to_dict(self, props, schema):
+        """
+        Converts the provided property list into a dict corresponding to the key
+        names (which may have identifiers)
+        """
+        result = {}
+        for i, tt_data in enumerate(schema):
+            tt, name, tt_type = tt_data
+            # If there is an alias, create a key from it; i.e. if the type
+            # identifier is t, then t.a works.
+            result[name] = (props[i], tt_type)
+        return result
+
     def execute(self, storage_manager):
         """
         Inserts a relationship into the storage layer.
@@ -54,6 +65,11 @@ class InsertRelationCommand(Command):
         :param storage_manager: storage manager for this instance
         :return: List of inserted relations
         """
+        relation_info = []
+        left_queries = []
+        right_queries = []
+        type_names = {}
+
         inserted_relations = []
         for rel, query1, query2 in self.relprops:
             # Gather information about the relation: name, type, schema and properties
@@ -77,21 +93,58 @@ class InsertRelationCommand(Command):
             qc1 = Query.parse_chain(storage_manager, queries1, type_schema1)
             qc2 = Query.parse_chain(storage_manager, queries2, type_schema2)
 
-            # Create iterators for the left and right nodes
-            iter1 = NodeIterator(storage_manager, MatchNode(None, type1), type_schema1, queries=qc1)
-            iter2 = NodeIterator(storage_manager, MatchNode(None, type2), type_schema2, queries=qc2)
+            # Store this data in a place for later usage; we just want to know
+            # which queries were on either side of a relation
+            left_queries.append((qc1, type_data1.index))
+            right_queries.append((qc2, type_data2.index))
+            relation_info.append({
+                "name": rel_name,
+                "type": rel_type,
+                "props": rel_props,
+                "left": [],
+                "right": []
+            })
+            type_names[type1] = (type_data1, type_schema_data1)
+            type_names[type2] = (type_data2, type_schema_data2)
 
+        # Iterate through each needed node type ONCE and do all queries there;
+        # this puts the relevant nodes on the correct side of their relation
+        # if they apply.
+        for tn, t in type_names.items():
+            td, t_sch = t
+            for np in storage_manager.get_nodes_of_type(td):
+                nodeType = np.node.nodeType
+                props = self.prop_to_dict(np.properties, t_sch)
+                for i, lr in enumerate(zip(left_queries, right_queries)):
+                    l_pair, r_pair = lr
+
+                    # Check left side
+                    l_query, l_type = l_pair
+                    if l_type == nodeType and l_query.test(props):
+                        relation_info[i]["left"].append(np.node)
+
+                    # Check right side
+                    r_query, r_type = r_pair
+                    if r_type == nodeType and r_query.test(props):
+                        relation_info[i]["right"].append(np.node)
+
+        # Actually insert the relations here
+        for rel_info in relation_info:
+            rel_name = rel_info["name"]
+            rel_type = rel_info["type"]
+            rel_props = rel_info["props"]
+            pairs = itertools.product(rel_info["left"], rel_info["right"])
             # Iterate over a product of the left node-set and right node-set. For
             # each, we will create a relation. We don't care if a node appears on
             # both sides; that's fine.
-            for np1, np2 in itertools.product(iter1, iter2):
-                if np1 == np2:
+            for left_node, right_node in pairs:
+                if left_node == right_node:
                     continue
 
                 self.logger.debug("Inserting relation %s between %s and %s"
-                                  % (rel_name, np1.node, np2.node))
+                                  % (rel_name, left_node, right_node))
                 rel = storage_manager.insert_relation(rel_type, rel_props,
-                    np1.node, np2.node)
+                    left_node, right_node)
                 inserted_relations.append(rel)
 
         return inserted_relations
