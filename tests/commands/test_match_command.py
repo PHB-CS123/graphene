103e858cdddae981a864ee8613b5ef6f15fef734
Eugene Bulkin <ebulkin@caltech.edu>
2015-06-09 16:32:58 -0700

    Add tests for LIMIT clause

--- tests/commands/test_match_command.py
+++ tests/commands/test_match_command.py
@@ -19,6 +19,10 @@ class TestMatchCommand(unittest.TestCase):
         cls.sm = cls.server.storage_manager
         cls.server.doCommands("CREATE TYPE T ( a: int );", False)
         cls.server.doCommands("INSERT NODE T(1), T(2), T(3), T(4), T(5);", False)
+        cls.server.doCommands("CREATE RELATION R ();", False)
+        cls.server.doCommands("INSERT RELATION T(a=1)-[R]->T(a=2), "
+                              "T(a=1)-[R]->T(a=3), T(a=1)-[R]->T(a=4), "
+                              "T(a=1)-[R]->T(a=5), T(a=2)-[R]->T(a=3)", False)
 
         cls.devnull = open(os.devnull, "w")
 
@@ -113,3 +117,53 @@ class TestMatchCommand(unittest.TestCase):
         cmd = self.server.parseString("MATCH (t:T) WHERE t2.a = 1;")[0]
         with self.assertRaises(Exception):
             cmd.execute(self.sm, self.devnull)
+
+    def test_limit_node(self):
+        # WARNING: Limits just take the elements in the order they were added
+        # to the database!! There is no real meaning to the order that you get.
+
+        # Limit of zero means just everything
+        cmd = self.server.parseString("MATCH (t:T) LIMIT 0")[0]
+
+        exp_vals = [ [1], [2], [3], [4], [5] ]
+        ret_vals = cmd.execute(self.sm, self.devnull)
+        self.assertListEqualUnsorted(ret_vals, exp_vals)
+
+        # Limit less than number of rows
+        cmd = self.server.parseString("MATCH (t:T) LIMIT 2")[0]
+
+        exp_vals = [ [1], [2] ]
+        ret_vals = cmd.execute(self.sm, self.devnull)
+        self.assertListEqualUnsorted(ret_vals, exp_vals)
+
+        # Limit more than number of rows (same as zero)
+        cmd = self.server.parseString("MATCH (t:T) LIMIT 30")[0]
+
+        exp_vals = [ [1], [2], [3], [4], [5] ]
+        ret_vals = cmd.execute(self.sm, self.devnull)
+        self.assertListEqualUnsorted(ret_vals, exp_vals)
+
+    def test_limit_rel(self):
+        # WARNING: Limits just take the elements in the order they were added
+        # to the database!! There is no real meaning to the order that you get.
+        #
+        # Limit of zero means just everything
+        cmd = self.server.parseString("MATCH (t:T)-[R]->(t2:T) LIMIT 0")[0]
+
+        exp_vals = [ [1, 2], [1, 3], [1, 4], [1, 5], [2, 3] ]
+        ret_vals = cmd.execute(self.sm, self.devnull)
+        self.assertListEqualUnsorted(ret_vals, exp_vals)
+
+        # Limit less than number of rows
+        cmd = self.server.parseString("MATCH (t:T)-[R]->(t2:T) LIMIT 2")[0]
+
+        exp_vals = [ [1, 2], [1, 3] ]
+        ret_vals = cmd.execute(self.sm, self.devnull)
+        self.assertListEqualUnsorted(ret_vals, exp_vals)
+
+        # Limit more than number of rows (same as zero)
+        cmd = self.server.parseString("MATCH (t:T)-[R]->(t2:T) LIMIT 30")[0]
+
+        exp_vals = [ [1, 2], [1, 3], [1, 4], [1, 5], [2, 3] ]
+        ret_vals = cmd.execute(self.sm, self.devnull)
+        self.assertListEqualUnsorted(ret_vals, exp_vals)
