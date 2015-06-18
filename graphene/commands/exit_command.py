dce404d65f1f2b8f297cfa066210b885621d38d0
Eugene Bulkin <ebulkin@caltech.edu>
2015-06-18 13:34:33 -0700

    Fix EXIT command to have execute method for abstract class

--- graphene/commands/exit_command.py
+++ graphene/commands/exit_command.py
@@ -3,3 +3,7 @@ from graphene.commands.command import Command
 class ExitCommand(Command):
     def __init__(self):
         pass
+
+    def execute(self, storage_manager, timer=None):
+        # This should never be used anyway.
+        pass
