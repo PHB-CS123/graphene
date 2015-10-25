42108dc722ab89400e9c8e947e5b9527145f6930
dpena <dpena@caltech.edu>
2015-10-25 03:11:33 -0700

    Misc. cleanup. Mostly changed execute base class signature to include output so that end to end tests can be written

--- graphene/storage/storage_manager.py
+++ graphene/storage/storage_manager.py
@@ -8,6 +8,7 @@ from graphene.storage.intermediate import *
 from graphene.storage.defrag.defrag_helpers import *
 
 
+
 class StorageManager:
     # Maximum size of the cache (in items)
     MAX_CACHE_SIZE = 10000
