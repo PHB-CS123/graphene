52541e82bf2a44ddb98c89725ce4a62f0b39d9e4
dpena <dpena@caltech.edu>
2015-11-28 19:12:21 -0800

    Added a ConfigStore class for storing configurations

---
+++ graphene/storage/config/config_store.py
@@ -0,0 +1,110 @@
+from enum import Enum
+from ConfigParser import ConfigParser
+import logging
+import os
+
+from storage.base.graphene_store import GrapheneStore
+
+
+class ConfigStore(object):
+    CONF_FILENAME = "default.conf"
+
+    class ConfigType(Enum):
+        """ Configuration value type. """
+        undefined = 0
+        int = 1
+        bool = 2
+        string = 3
+
+    def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.file_path = GrapheneStore().datafilesDir + self.CONF_FILENAME
+        try:
+            # If no config file exists, create one
+            if not os.path.isfile(self.file_path):
+                open(self.file_path, "w+").close()
+            self.config = ConfigParser()
+            self.config.read(self.file_path)
+        except IOError:
+            raise IOError("ERROR: unable to open file: " + self.file_path)
+
+    def __del__(self):
+        """ Writes configs back to the config file """
+        config_file = open(self.file_path)
+        self.config.write(config_file)
+        config_file.close()
+
+    def get_config(self, name, config_type, section="Main"):
+        """
+        Get the value for the given config name
+
+        :param name: Name of the value to get
+        :type name: str
+        :param config_type: Type of value
+        :type config_type: ConfigStore.ConfigType
+        :param section: Section where the value is located
+        :type section: str
+        :return: Value for the config
+        :rtype: int | bool | str
+        """
+        value = self.config_section_map(section)[name]
+        return self.convert_value_to_type(value, config_type)
+
+    def set_config(self, name, value, section="Main"):
+        """
+        Set the value for the given config name
+
+        :param name: Name of the value to set
+        :type name: str
+        :param value: Value for the given name
+        :type value: int | bool | str
+        :param section: Section to write the value to
+        :type section: str
+        :return: Nothing
+        :rtype: None
+        """
+        if not self.config.has_section(section):
+            self.config.add_section(section)
+        self.config.set(section, name, value)
+
+    def convert_value_to_type(self, value, config_type):
+        """
+        Convert the given value to the config type
+
+        :param value: Value to convert
+        :type value: str
+        :param config_type: Config type to convert to
+        :type config_type: ConfigStore.ConfigType
+        :return: Converted value
+        :rtype: int | bool | str
+        """
+        if config_type is self.ConfigType.int:
+            return int(value)
+        elif config_type is self.ConfigType.bool:
+            return bool(value)
+        elif config_type is self.ConfigType.string:
+            return value
+        else:
+            raise TypeError("Unhandled ConfigType: %s" % config_type)
+
+    def config_section_map(self, section):
+        """
+        Get a dictionary with the values for the given section
+
+        :param section: Section to get values for
+        :type section: str
+        :return: Dictionary mapping names to values for the section
+        :rtype: dict[str, str]
+        """
+        values = {}
+        options = self.config.options(section)
+        for option in options:
+            try:
+                values[option] = self.config.get(section, option)
+                if values[option] == -1:
+                    self.logger.debug("Unable to find section: %s" % option)
+            except Exception, e:
+                self.logger.warning("Caught exception %s when looking through "
+                                    "section %s" % (e, section))
+                values[option] = None
+        return values
